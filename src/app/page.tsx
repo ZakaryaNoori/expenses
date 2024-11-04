8badca83dee102f4981e1a3a79867d977ea2c6c8
Zakarya Noori <66834670+ZakaryaNoori@users.noreply.github.com>
2024-11-03 23:23:00 -0500

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,44 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="grid lg:grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full md:w-auto">
+        <table className="w-full">
+          <tbody>
+            <tr>
+              <td className="border p-2">
+                <h1 className="text-lg">Rent:</h1>
+              </td>
+              <td className="border p-2">
+                <b>1756.64</b>
+              </td>
+            </tr>
+            <tr>
+              <td className="border p-2">
+                <h1 className="text-lg">Electricity:</h1>
+              </td>
+              <td className="border p-2">
+                <b>81.16</b>
+              </td>
+            </tr>
+            <tr>
+              <td className="border p-2">
+                <h1 className="text-lg">Total:</h1>
+              </td>
+              <td className="border p-2">
+                <b>1837.80</b>
+              </td>
+            </tr>
+            <tr>
+              <td className="border p-2">
+                <h1 className="text-lg">Per Person:</h1>
+              </td>
+              <td className="border p-2">
+                <b>{1837.8 / 3}</b>
+              </td>
+            </tr>
+          </tbody>
+        </table>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
@@ -50,51 +68,7 @@ export default function Home() {
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        @2024 Zakarya Noori
       </footer>
     </div>
   );
